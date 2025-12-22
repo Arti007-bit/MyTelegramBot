d11e21b64816dc262e0197700dcda804e5565742
Arti007-bit <artizeron007@gmail.com>
2025-12-22 13:01:14 +0330

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,27 @@ TOKEN = os.environ["TOKEN"]
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+
+# /start handler
 @dp.message(CommandStart())
 async def start_handler(message: Message):
     await message.answer("سلام! ربات با موفقیت روی Render اجرا شد ✅")
 
+
+# delete links in groups
+@dp.message()
+async def delete_links(message: Message):
+    if message.chat.type in ("group", "supergroup"):
+        if message.entities:
+            for entity in message.entities:
+                if entity.type in ("url", "text_link"):
+                    await message.delete()
+                    return
+
+
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
